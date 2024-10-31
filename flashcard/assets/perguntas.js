e1d0581a7adea707560c3f25a94f20e1f94ddcdd
leandro0080 <146841756+leandro0080@users.noreply.github.com>
2024-10-31 12:16:31 -0300

    Update perguntas.js

--- flashcard/assets/perguntas.js
+++ flashcard/assets/perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Animais',
+    'Qual animal dorme de cabeça pra baixo?',
+    'O morcego'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital do Paraná?',
-    'A capital do Paraná é Curitiba'
+    'Qual a capital do Brasil?',
+    'Brasília'
 )
 
 criaCartao(
     'Futebol',
-    'Que time ganhou a Libertadores de 2011?',
-    'O time campeão foi o brasileiro Santos'
+    'Que jogador é conhecido como rei?',
+    'Pelé'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz obrigado em inglês?',
+    'Thank you'
 )
